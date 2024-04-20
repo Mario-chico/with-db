04cc97aed7e21c466bec5e4c4c5985f3a3f0cf55
Mario Chico <chico.developer@hotmail.com>
2024-04-19 20:55:28 -0600

    Definiendo tabla Reactions

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,12 @@
-import { defineDb } from 'astro:db';
+import { defineDb, column, defineTable } from 'astro:db';
 
+const Reactions = defineTable({
+  columns: {
+    id: column.number({primaryKey: true}),
+    title: column.text(),
+  }
+})
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Reactions }
 });
