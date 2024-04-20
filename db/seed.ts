4f07ccd31f672e4baa5a5053d95ed7db5946aeb0
Mario Chico <chico.developer@hotmail.com>
2024-04-19 21:03:37 -0600

    Agregando semilla a la db

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,10 @@
-import { db } from 'astro:db';
+import { db, Reactions } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
+	await db.insert(Reactions).values([
+		{id: 1, title: 'first-post'},
+		{id: 2, title: 'second-post'},
+	])
 }
