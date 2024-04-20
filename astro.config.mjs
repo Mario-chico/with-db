b4b21ab56a8bb8c9fc500c574e68981f1cc8eb3b
Mario Chico <chico.developer@hotmail.com>
2024-04-19 21:37:47 -0600

    Trayendo los datos de la tabla Reactions en la pagina de los blogs para visualizar y probar como es el select

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), db()]
+  integrations: [mdx(), sitemap(), db()],
+  output: 'hybrid',
 });
