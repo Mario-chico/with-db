0b502288666d8caf2843caa61116019bcd7f01b4
Mario Chico <chico.developer@hotmail.com>
2024-04-23 20:44:36 -0600

    Cambiando la api, componente y blogs para que funcione con el boton de fire

--- src/pages/api/addReaction.ts
+++ src/pages/api/addReaction.ts
@@ -1,23 +1,32 @@
 import type { APIRoute } from "astro";
-import { Reactions, db } from "astro:db";
+import { Reactions, db, eq } from "astro:db";
 
 export const prerender = false;
 
 export const POST: APIRoute = async ({request}) => {
-  const {slug} = await request.json();
-
+  const {slug, fire} = await request.json();
+console.log('Si me estoy ejecutando');
   if(!slug) {
     return new Response(null, {
       status: 404,
-      statusText: "Nombre no"
+      statusText: "slig falta"
     });
   }
-  const req = await db.insert(Reactions).values({slug});
-
-  return new Response(JSON.stringify({ success: true}), {
+  if(!fire) {
+    return new Response(null, {
+      status: 404,
+      statusText: "falta fire"
+    });
+  }
+  const fireLike = await db.select().from(Reactions).where(eq(Reactions.slug, slug)).get()
+  console.log('Si me estoy ejecutando');
+  const update = db.update(Reactions).set({fire: (fireLike?.fire ?? 0) +1}).where(eq(Reactions.slug, slug)).returning().get();
+  return new Response(JSON.stringify(update), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
     },
   });
+  
+  
 };
